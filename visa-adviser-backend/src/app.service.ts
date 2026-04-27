219255d6f1fa2e2aceb0461065c7fcc467cee7be
farhat sattar <farhatsattar8675@gmail.com>
2026-04-27 13:24:47 +0500

    Added backend routes

--- visa-adviser-backend/src/app.service.ts
+++ visa-adviser-backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Worldwide Visa Adviser API is running';
   }
 }
